9f4f573b01f61288a5c2bb2308b236760d64e019
hagerezz <queen.emilly12@gmail.com>
2024-10-03 12:30:30 +0300

    hi

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+
+"""Script that reads stdin line by line and computes metrics"""
+
 import sys
 
 
-def prints(d, size):
-    """ WWprint the info"""
+def printsts(dic, size):
+    """ WWPrints information """
     print("File size: {:d}".format(size))
-    for i in sorted(d.keys()):
-        if d[i] != 0:
-            print("{}: {:d}".format(i, d[i]))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-s = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
        "404": 0, "405": 0, "500": 0}
 
 count = 0
@@ -20,24 +22,24 @@ size = 0
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            prints(s, size)
+            printsts(sts, size)
 
-        slist = line.split()
+        stlist = line.split()
         count += 1
 
         try:
-            size += int(slist[-1])
+            size += int(stlist[-1])
         except:
             pass
 
         try:
-            if slist[-2] in s:
-                s[slist[-2]] += 1
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
         except:
             pass
-    prints(s, size)
+    printsts(sts, size)
 
 
 except KeyboardInterrupt:
-    prints(s, size)
+    printsts(sts, size)
     raise
